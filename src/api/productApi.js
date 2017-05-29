2f3a809582535e514cf917b586df2de7ec7da61c
Eaudran <nevosis@gmail.com>
2017-05-29 10:28:50 +0200

    ajout de l extension reduxTool sur chrome et la compatibilité sur le createStore

--- src/api/productApi.js
+++ src/api/productApi.js
@@ -15,14 +15,19 @@ var productList = [{ id: 0, name: "haha" }, { id: 1, name: "johnny deep" }];
 export function getProducts() {
 	var clonedArray = JSON.parse(JSON.stringify(productList));
 
+	console.log("[productApi.getProducts] START");
+
+
 	store.dispatch(requestProducts());
 	setTimeout(() => {
+		console.log("[productApi.getProducts] END, dispatch ", clonedArray);
 		store.dispatch(getProductsSuccess(clonedArray));
 	}, 750);
 }
 
 export function addProduct(name) {
 	store.dispatch(requestAddProduct());
+	console.log("[productApi.addProduct] START, name ", name);
 
 	productList.push({
 		id: productList[productList.length - 1].id + 1,
@@ -30,6 +35,7 @@ export function addProduct(name) {
 	});
 	var clonedArray = JSON.parse(JSON.stringify(productList));
 	setTimeout(() => {
+		console.log("[productApi.addProduct] END, productList ", productList);
 		store.dispatch(addProductSuccess(clonedArray));
 	}, 750);
 }
